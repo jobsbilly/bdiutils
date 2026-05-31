43f10a2a8b4076b776474ff5481d86c9f949a25f
Ben Issersohn <jobsbilly@gmail.com>
2026-05-31 14:45:12 -0700

    Add a function that initializes/configures the logging handler(s) I use often.

--- src/bdiutils/logging.py
+++ src/bdiutils/logging.py
@@ -30,3 +30,26 @@ class TZAwareFormatter(logging.Formatter):
 
 def log_formatter(fmt: str, zone: str = "America/Los_Angeles") -> logging.Formatter:
     return TZAwareFormatter(tz=pytz.timezone(zone), fmt=fmt, datefmt="%Y-%m-%d %H:%M:%S.%f %Z")
+
+def init_loggers(log_file_path: str | pathlib.Path | None, level=logging.INFO, attr: str = "name") -> None:
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(level)
+    console_handler.setFormatter(log_formatter(f"%(created)f %(levelname)8s [%({attr})7s:%(lineno)-3d] %(message)s"))
+
+    handlers = [console_handler]
+
+    min_level = level if level < logging.INFO else logging.INFO
+
+    if log_file_path is not None:
+        log_file_path = pathlib.Path(log_file_path)
+        if not log_file_path.exists():
+            raise FileNotFoundError(log_file_path)
+        # Append a separator line with a blank line before and after.
+        with open(log_file_path, mode="a", encoding="utf-8") as log_file:
+            log_file.write("\n".join(["_" * 80, "_" * 80, "\n"]))
+        file_handler = logging.FileHandler(log_file_path, mode="a", encoding="utf-8")
+        file_handler.setLevel(min_level)
+        file_handler.setFormatter(log_formatter(f"%(asctime)s %(levelname)8s [%({attr})10s:%(lineno)-3d] %(message)s"))
+        handlers.append(file_handler)
+
+    logging.basicConfig(level=min_level, handlers=handlers, force=True)
